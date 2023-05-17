20964390db785fb43b3adaadc4dbc3cb7fa32da4
风默1314 <360822587@qq.com>
2023-05-17 22:58:24 +0800

    1111

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig,splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import alias from "@rollup/plugin-alias";
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [alias(),vue(),
   // el推荐按需导入插件1
   AutoImport({
     resolvers: [ElementPlusResolver()],
@@ -26,22 +27,22 @@ export default defineConfig({
     'process.env': process.env
   },
   // 打包配置
-  build: {
-    // target: 'modules',
-    // outDir: "../webapp/",
-    brotliSize:false,
-    chunkSizeWarningLimit: 1500,
-    emptyOutDir:false,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  },
+  // build: {
+  //   // target: 'modules',
+  //   // outDir: "../webapp/",
+  //   brotliSize:false,
+  //   chunkSizeWarningLimit: 1500,
+  //   emptyOutDir:false,
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           return id.toString().split('node_modules')[1].split('/')[0].toString();
+  //         }
+  //       }
+  //     }
+  //   }
+  // },
   //本地服务
   server: {
     port:80,//端口
@@ -50,8 +51,8 @@ export default defineConfig({
     // 跨域代理配置
     proxy: {
       '/api': {
-        target: 'http://8.130.22.229:8086/lh.com/',
-        // target:'http://localhost:8086/lh.com',
+        // target: 'http://8.130.22.229:8086/lh.com/',
+        target:'http://localhost:8086/lh.com',
         changeOrigin: true,
         rewrite: (path) => {
           console.log(path);
